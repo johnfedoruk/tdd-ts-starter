0f46d6606a24ec8cfe453bdbd96b4730b0ea3df0
John A. Fedoruk <johnny@johnfedoruk.ca>
2017-07-07 22:42:17 +0100

    Added tsc config

---
+++ js/index.js
@@ -0,0 +1,22 @@
+System.register("hello-world", [], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var HelloWorld;
+    return {
+        setters: [],
+        execute: function () {
+            HelloWorld = (function () {
+                function HelloWorld(name) {
+                    this.name = name;
+                }
+                HelloWorld.prototype.sayHello = function () {
+                    var greeting = "Hello, " + this.name + "!";
+                    return greeting;
+                };
+                return HelloWorld;
+            }());
+            exports_1("HelloWorld", HelloWorld);
+        }
+    };
+});
+//# sourceMappingURL=index.js.map
